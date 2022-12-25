3fea49927ec51dca10a48af5a7b72189819c8338
AkaraChen <85140972+AkaraChen@users.noreply.github.com>
2022-12-25 09:46:23 +0800

    feat: optimize workflow

--- main.go
+++ main.go
@@ -159,11 +159,11 @@ func main() {
 		os.WriteFile(file, result, fs.ModeDevice)
 	}
 	pterm.Info.Printfln("Generate changelog...")
+	run("conventional-changelog", "-p", "angular", "-i", "CHANGELOG.md", "-s")
 	run("git", "add", ".")
 	run("git", "commit", "-m", "release: "+versionString)
-	pterm.Info.Printfln("Push your change...")
 	run("git", "tag", versionString)
-	run("conventional-changelog", "-p", "angular", "-i", "CHANGELOG.md", "-s")
+	pterm.Info.Printfln("Push your change...")
 	run("git", "push")
 	confirmPublish, _ := pterm.
 		DefaultInteractiveConfirm.
